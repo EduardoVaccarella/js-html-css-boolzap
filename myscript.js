690ad2542896c360c9c443ecf755f41e85e05db2
eduardoego <eduardo0vaccarella@gmail.com>
2022-03-03 13:07:46 +0100

    finito la milestone 3 ma manca qualcosa sui messaggi

--- myscript.js
+++ myscript.js
@@ -86,7 +86,122 @@ const app = new Vue(
                     }
                 ],
             },
-            ]
+            ],
+            
+            contatto: "",
+            messaggio: "",
+
+            currentChat: 0,
+
+            currentMessages: [
+                {
+                    date: '10/01/2020 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:15:22',
+                    text: 'Tutto fatto!',
+                    status: 'received'
+                }
+            ],
+
+            currentName: 'Michele'
+
         },
+        methods: {
+            displayMessages: function(index2) {
+
+                this.currentChat = index2;
+
+                this.currentName = this.contacts[index2].name;
+
+                this.currentMessages = [];
+
+                for(let i = 0; i < this.contacts[index2].messages.length; i++) {
+
+                    this.currentMessages.push({
+                        date: this.contacts[index2].messages[i].date,
+                        text: this.contacts[index2].messages[i].text,
+                        status: this.contacts[index2].messages[i].status
+                    });
+                }
+                
+            },
+
+            sendMessage: function() {
+
+                if(!this.messaggio == '') {
+                    let today = new Date();
+                    currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+                    this.contacts[this.currentChat].messages.push(
+                        {
+                            date: currentTime,
+                            text: this.messaggio,
+                            status: 'sent'
+                        }
+                    );
+
+                    this.currentMessages.push(
+                        {
+                            date: currentTime,
+                            text: this.messaggio,
+                            status: 'sent'
+                        }
+                    );
+
+                    this.messaggio = '';
+
+                    let currentTimeChat = this.currentChat;
+
+                    setTimeout(() => {
+
+                        let today = new Date();
+                        currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+                        this.contacts[currentTimeChat].messages.push(
+                            {
+                                date: currentTime,
+                                text: 'Okay',
+                                status: 'received'
+                            }
+                        );
+
+                        if(currentTimeChat == this.currentChat) {
+                            this.currentMessages.push(
+                                {
+                                    date: currentTime,
+                                    text: 'Okay',
+                                    status: 'received'
+                                }
+                            );
+                        }
+                        
+                    } , 1000);
+                }
+            },
+
+            cercaContatto: function() {
+                if(!this.contatto == '') {
+
+
+
+                    console.log(this.contatto);
+                    this.contatto = '';
+                }
+            }
+        }
     }
 )
+
+
+
+// con l'operatore terziario verifico se un messaggio é inviato o ricevuto e assegno la classe corrispondente al messaggio
+
+// nei methods scriveró la funzione per ricercare i contatti che metterá in display: none; i contatti che non includono nel nome la parola ricercata
